e17a80a8fd87c8926d8df9cb9501bfb34d03196e
Rob Smith <merlin@sleepingkingstudios.com>
2020-05-09 00:24:24 -0400

    Clean up Toolbox::Mixin.

--- spec/sleeping_king_studios/tools/toolbox/mixin_spec.rb
+++ spec/sleeping_king_studios/tools/toolbox/mixin_spec.rb
@@ -1,90 +1,70 @@
-# spec/sleeping_king_studios/tools/toolbox/mixin_spec.rb
+# frozen_string_literal: true
 
 require 'spec_helper'
 
 require 'sleeping_king_studios/tools/toolbox/mixin'
 
 RSpec.describe SleepingKingStudios::Tools::Toolbox::Mixin do
+  extend RSpec::SleepingKingStudios::Concerns::ExampleConstants
+
   shared_context 'when the mixin includes another mixin' do
-    around(:example) do |example|
-      begin
-        module Spec
-          module SecondMixin
-            extend SleepingKingStudios::Tools::Toolbox::Mixin
+    example_constant 'Spec::SecondMixin' do
+      Module.new do
+        extend  SleepingKingStudios::Tools::Toolbox::Mixin
+        include Spec::FirstMixin
 
-            include Spec::FirstMixin
+        self::ClassMethods.define_method(:second_class_method) { :ni }
 
-            module ClassMethods
-              def second_class_method; :ni; end
-            end # module
-
-            def second_instance_method; :ni; end
-          end # module
-        end # module
-
-        example.call
-      ensure
-        Spec.send :remove_const, :SecondMixin
-      end # begin-ensure
-    end # around
+        define_method(:second_instance_method) { :ni }
+      end
+    end
 
     let(:described_class) do
       Class.new.tap { |klass| klass.include Spec::SecondMixin }
-    end # let
-  end # shared_context
+    end
+  end
 
   shared_context 'when the mixin overrides another mixin' do
-    around(:example) do |example|
-      begin
-        module Spec
-          module ThirdMixin
-            extend SleepingKingStudios::Tools::Toolbox::Mixin
+    example_constant 'Spec::ThirdMixin' do
+      Module.new do
+        extend  SleepingKingStudios::Tools::Toolbox::Mixin
+        include Spec::FirstMixin
 
-            include Spec::FirstMixin
+        self::ClassMethods.define_method(:first_class_method) do
+          [super(), :san]
+        end
 
-            module ClassMethods
-              def first_class_method; [super, :san]; end
-            end # module
-
-            def first_instance_method; [super, :san]; end
-          end # module
-        end # module
-
-        example.call
-      ensure
-        Spec.send :remove_const, :ThirdMixin
-      end # begin-ensure
-    end # around
+        define_method(:first_instance_method) { [super(), :san] }
+      end
+    end
 
     let(:described_class) do
       Class.new.tap { |klass| klass.include Spec::ThirdMixin }
-    end # let
-  end # shared_context
+    end
+  end
 
   let(:described_class) do
     Class.new.tap { |klass| klass.include Spec::FirstMixin }
-  end # let
+  end
   let(:instance) { described_class.new }
 
-  around(:example) do |example|
-    begin
-      module Spec
-        module FirstMixin
-          extend SleepingKingStudios::Tools::Toolbox::Mixin
+  example_constant 'Spec::FirstMixin' do
+    Module.new do
+      extend SleepingKingStudios::Tools::Toolbox::Mixin
 
-          module ClassMethods
-            def first_class_method; :ichi; end
-          end # module
+      def first_instance_method
+        :ichi
+      end
+    end
+  end
 
-          def first_instance_method; :ichi; end
-        end # module
-      end # module
-
-      example.call
-    ensure
-      Spec.send :remove_const, :FirstMixin
-    end # begin-ensure
-  end # around
+  example_constant 'Spec::FirstMixin::ClassMethods' do
+    Module.new do
+      def first_class_method
+        :ichi
+      end
+    end
+  end
 
   describe 'should inherit class methods' do
     describe '::first_class_method' do
@@ -92,18 +72,22 @@ RSpec.describe SleepingKingStudios::Tools::Toolbox::Mixin do
 
       it { expect(described_class.first_class_method).to be :ichi }
 
-      wrap_context 'when the mixin includes another mixin' do
+      context 'when the mixin includes another mixin' do
+        include_context 'when the mixin includes another mixin'
+
         it { expect(described_class).to respond_to(:first_class_method) }
 
         it { expect(described_class.first_class_method).to be :ichi }
-      end # wrap_context
+      end
 
-      wrap_context 'when the mixin overrides another mixin' do
+      context 'when the mixin overrides another mixin' do
+        include_context 'when the mixin overrides another mixin'
+
         it { expect(described_class).to respond_to(:first_class_method) }
 
-        it { expect(described_class.first_class_method).to be == [:ichi, :san] }
-      end # wrap_context
-    end # describe
+        it { expect(described_class.first_class_method).to be == %i[ichi san] }
+      end
+    end
 
     describe '::second_class_method' do
       it { expect(described_class).not_to respond_to(:second_class_method) }
@@ -112,9 +96,9 @@ RSpec.describe SleepingKingStudios::Tools::Toolbox::Mixin do
         it { expect(described_class).to respond_to(:second_class_method) }
 
         it { expect(described_class.second_class_method).to be :ni }
-      end # wrap_context
-    end # describe
-  end # describe
+      end
+    end
+  end
 
   describe 'should inherit instance methods' do
     describe '#first_instance_method' do
@@ -122,18 +106,22 @@ RSpec.describe SleepingKingStudios::Tools::Toolbox::Mixin do
 
       it { expect(instance.first_instance_method).to be :ichi }
 
-      wrap_context 'when the mixin includes another mixin' do
+      context 'when the mixin includes another mixin' do
+        include_context 'when the mixin includes another mixin'
+
         it { expect(instance).to respond_to(:first_instance_method) }
 
         it { expect(instance.first_instance_method).to be :ichi }
-      end # wrap_context
+      end
 
-      wrap_context 'when the mixin overrides another mixin' do
+      context 'when the mixin overrides another mixin' do
+        include_context 'when the mixin overrides another mixin'
+
         it { expect(instance).to respond_to(:first_instance_method) }
 
-        it { expect(instance.first_instance_method).to be == [:ichi, :san] }
-      end # wrap_context
-    end # describe
+        it { expect(instance.first_instance_method).to be == %i[ichi san] }
+      end
+    end
 
     describe '#second_instance_method' do
       it { expect(instance).not_to respond_to(:second_instance_method) }
@@ -142,7 +130,7 @@ RSpec.describe SleepingKingStudios::Tools::Toolbox::Mixin do
         it { expect(instance).to respond_to(:second_instance_method) }
 
         it { expect(instance.second_instance_method).to be :ni }
-      end # wrap_context
-    end # describe
-  end # describe
-end # describe
+      end
+    end
+  end
+end
