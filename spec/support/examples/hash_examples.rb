6151380bd0ec04d03260bcb3babdca87a82d0561
Rob Smith <merlin@sleepingkingstudios.com>
2020-07-13 18:58:45 -0400

    Clean up RuboCop warnings in HashExamples.

--- spec/support/examples/hash_examples.rb
+++ spec/support/examples/hash_examples.rb
@@ -1,4 +1,4 @@
-# spec/support/examples/hash_examples.rb
+# frozen_string_literal: true
 
 require 'support/examples'
 
@@ -8,71 +8,80 @@ module Spec::Examples
 
     shared_context 'when the hash is frozen' do
       let(:hsh) { super().freeze }
-    end # shared_context
+    end
 
     shared_context 'when the hash keys are frozen' do
       before(:example) do
         hsh.each { |key, _| key.freeze }
-      end # before example
-    end # shared_context
+      end
+    end
 
     shared_context 'when the hash values are frozen' do
       before(:example) do
         hsh.each { |_, value| value.freeze }
-      end # before example
-    end # shared_context
+      end
+    end
 
     shared_examples 'should create a deep copy of a hash' do
       describe 'with a hash with mutable keys' do
         let(:key_class) { Struct.new :value }
-        let(:hsh)       { { key_class.new('foo') => 'foo', key_class.new('bar') => 'bar', key_class.new('baz') => 'baz' } }
-        let(:cpy)       { instance.deep_dup hsh }
+        let(:hsh) do
+          {
+            key_class.new(+'foo') => 'foo',
+            key_class.new(+'bar') => 'bar',
+            key_class.new(+'baz') => 'baz'
+          }
+        end
+        let(:cpy) { instance.deep_dup hsh }
 
-        def normalize hsh
+        def normalize(hsh)
           hsh.each.with_object({}) do |(key, value), copy|
             normalized = key.value.dup
 
             copy[normalized] = value
-          end # each
+          end
           hsh.keys.map(&:value).map(&:dup)
-        end # method normalize
+        end
 
         it { expect(cpy).to be == hsh }
 
         it { expect(normalize cpy).to be == normalize(hsh) }
 
-        it 'should return a copy of the hash' do
-          expect { cpy[key_class.new 'wibble'] = 'wibble' }.not_to change { normalize hsh }
+        it 'should return a copy of the hash', :aggregate_failures do
+          expect { cpy[key_class.new 'wibble'] = 'wibble' }
+            .not_to(change { normalize hsh })
 
-          expect { cpy[key_class.new 'foo']    = 'FOO' }.not_to change { normalize hsh }
-        end # it
+          expect { cpy[key_class.new 'foo'] = 'FOO' }
+            .not_to(change { normalize hsh })
+        end
 
         it 'should return a copy of the hash values' do
-          expect { cpy[key_class.new 'foo']   << 'wobble' }.not_to change { normalize hsh }
-        end # it
+          expect { cpy[key_class.new 'foo'] << 'wobble' }
+            .not_to(change { normalize hsh })
+        end
 
         it 'should return a shallow copy of the hash keys' do
-          expect {
+          expect do
             cpy.keys.find { |key| key.value == 'foo' }.value << 'wobble'
-          }.to change { normalize hsh }
-        end # it
+          end.to(change { normalize hsh })
+        end
 
         context 'with a defined #deep_dup method' do
           before(:example) do
             key_class.send :define_method, :deep_dup do
               self.class.new(value.dup)
-            end # method deep_dup
-          end # before example
+            end
+          end
 
           it 'should return a deep copy of the hash keys' do
             copy = instance.deep_dup hsh
 
-            expect {
+            expect do
               copy.keys.find { |key| key.value == 'foo' }.value << 'wobble'
-            }.not_to change { normalize hsh }
-          end # it
-        end # context
-      end # describe
+            end.not_to(change { normalize hsh })
+          end
+        end
+      end
 
       describe 'with a hash with string keys' do
         let(:hsh) { { 'foo' => 'foo', 'bar' => 'bar', 'baz' => 'baz' } }
@@ -80,255 +89,313 @@ module Spec::Examples
 
         it { expect(cpy).to be == hsh }
 
-        it 'should return a copy of the hash' do
-          expect { cpy['wibble'] = 'wibble' }.not_to change { hsh }
+        it 'should return a copy of the hash', :aggregate_failures do
+          expect { cpy['wibble'] = 'wibble' }.not_to(change { hsh })
 
-          expect { cpy['foo']    = 'FOO' }.not_to change { hsh }
-        end # it
+          expect { cpy['foo'] = 'FOO' }.not_to(change { hsh })
+        end
 
         it 'should return a copy of the hash values' do
-          expect { cpy['foo']   << 'wobble' }.not_to change { hsh }
-        end # it
-      end # describe
+          expect { cpy['foo'] << 'wobble' }.not_to(change { hsh })
+        end
+      end
 
       describe 'with a hash with symbol keys' do
-        let(:hsh) { { :foo => 'foo', :bar => 'bar', :baz => 'baz' } }
+        let(:hsh) { { foo: 'foo', bar: 'bar', baz: 'baz' } }
         let(:cpy) { instance.deep_dup hsh }
 
         it { expect(cpy).to be == hsh }
 
-        it 'should return a copy of the hash' do
-          expect { cpy[:wibble] = 'wibble' }.not_to change { hsh }
+        it 'should return a copy of the hash', :aggregate_failures do
+          expect { cpy[:wibble] = 'wibble' }.not_to(change { hsh })
 
-          expect { cpy[:foo]    = 'FOO' }.not_to change { hsh }
-        end # it
+          expect { cpy[:foo] = 'FOO' }.not_to(change { hsh })
+        end
 
         it 'should return a copy of the hash values' do
-          expect { cpy[:foo]   << 'wobble' }.not_to change { hsh }
-        end # it
-      end # describe
+          expect { cpy[:foo] << 'wobble' }.not_to(change { hsh })
+        end
+      end
 
       describe 'with a hash with hash values' do
         let(:hsh) do
           {
-            :english => {
+            english:  {
               'one'   => '1',
               'two'   => '2',
               'three' => '3'
-            }, # end hash
-            :japanese => {
+            },
+            japanese: {
               'yon'  => '4',
               'go'   => '5',
               'roku' => '6'
-            }, # end hash
-            :spanish => {
+            },
+            spanish:  {
               'siete' => '7',
               'ocho'  => '8',
               'nueve' => '9'
-            } # end hash
-          } # end hash
-        end # let
+            }
+          }
+        end
         let(:cpy) { instance.deep_dup hsh }
 
         it { expect(cpy).to be == hsh }
 
-        it 'should return a copy of the hash' do
-          expect { cpy[:german]  = {} }.not_to change { hsh }
+        it 'should return a copy of the hash', :aggregate_failures do
+          expect { cpy[:german]  = {} }.not_to(change { hsh })
 
-          expect { cpy[:english] = {} }.not_to change { hsh }
-        end # it
+          expect { cpy[:english] = {} }.not_to(change { hsh })
+        end
 
-        it 'should return a copy of the child hashes' do
+        it 'should return a copy of the child hashes', :aggregate_failures do
           child = cpy[:english]
 
-          expect { child['ten'] = '10' }.not_to change { hsh }
+          expect { child['ten'] = '10' }.not_to(change { hsh })
 
-          expect { child['one'] = '1.0' }.not_to change { hsh }
-        end # it
+          expect { child['one'] = '1.0' }.not_to(change { hsh })
+        end
 
         it 'should return a copy of the child hash values' do
           child = cpy[:english]
 
-          expect { child['one'] << '.0' }.not_to change { hsh }
-        end # it
-      end # describe
-    end # shared_examples
+          expect { child['one'] << '.0' }.not_to(change { hsh })
+        end
+      end
+    end
 
     shared_examples 'should perform a deep freeze of the hash' do
       describe 'with a hash with mutable keys' do
         let(:key_class) { Struct.new :value }
-        let(:hsh)       { { key_class.new('foo') => 'foo', key_class.new('bar') => 'bar', key_class.new('baz') => 'baz' } }
+        let(:hsh) do
+          {
+            key_class.new('foo') => 'foo',
+            key_class.new('bar') => 'bar',
+            key_class.new('baz') => 'baz'
+          }
+        end
 
-        it 'should freeze the hash, keys, and values' do
-          expect { instance.deep_freeze hsh }.
-            to change(hsh, :frozen?).
-            to be true
+        it 'should freeze the hash' do
+          expect { instance.deep_freeze hsh }
+            .to change(hsh, :frozen?)
+            .to be true
+        end
 
-          hsh.each do |key, value|
-            expect(key.frozen?).to be true
-            expect(value.frozen?).to be true
-          end # each
-        end # it
-      end # describe
+        it 'should freeze the hash keys' do
+          instance.deep_freeze hsh
+
+          expect(hsh.keys.all?(&:frozen?)).to be true
+        end
+
+        it 'should freeze the hash values' do
+          instance.deep_freeze hsh
+
+          expect(hsh.values.all?(&:frozen?)).to be true
+        end
+      end
 
       describe 'with a hash with string keys' do
         let(:hsh) { { 'foo' => 'foo', 'bar' => 'bar', 'baz' => 'baz' } }
 
-        it 'should freeze the hash, keys, and values' do
-          expect { instance.deep_freeze hsh }.
-            to change(hsh, :frozen?).
-            to be true
+        it 'should freeze the hash' do
+          expect { instance.deep_freeze hsh }
+            .to change(hsh, :frozen?)
+            .to be true
+        end
 
-          hsh.each do |key, value|
-            expect(key.frozen?).to be true
-            expect(value.frozen?).to be true
-          end # each
-        end # it
-      end # describe
+        it 'should freeze the hash keys' do
+          instance.deep_freeze hsh
+
+          expect(hsh.keys.all?(&:frozen?)).to be true
+        end
+
+        it 'should freeze the hash values' do
+          instance.deep_freeze hsh
+
+          expect(hsh.values.all?(&:frozen?)).to be true
+        end
+      end
 
       describe 'with a hash with symbol keys' do
-        let(:hsh) { { :foo => 'foo', :bar => 'bar', :baz => 'baz' } }
+        let(:hsh) { { foo: 'foo', bar: 'bar', baz: 'baz' } }
 
-        it 'should freeze the hash and values' do
-          expect { instance.deep_freeze hsh }.
-            to change(hsh, :frozen?).
-            to be true
+        it 'should freeze the hash' do
+          expect { instance.deep_freeze hsh }
+            .to change(hsh, :frozen?)
+            .to be true
+        end
 
-          hsh.each { |_, value| expect(value.frozen?).to be true }
-        end # it
-      end # describe
+        it 'should freeze the hash keys' do
+          instance.deep_freeze hsh
+
+          expect(hsh.keys.all?(&:frozen?)).to be true
+        end
+
+        it 'should freeze the hash values' do
+          instance.deep_freeze hsh
+
+          expect(hsh.values.all?(&:frozen?)).to be true
+        end
+      end
 
       describe 'with a hash with hash values' do
         let(:hsh) do
           {
-            :english => {
+            english:  {
               'one'   => '1',
               'two'   => '2',
               'three' => '3'
-            }, # end hash
-            :japanese => {
+            },
+            japanese: {
               'yon'  => '4',
               'go'   => '5',
               'roku' => '6'
-            }, # end hash
-            :spanish => {
+            },
+            spanish:  {
               'siete' => '7',
               'ocho'  => '8',
               'nueve' => '9'
-            } # end hash
-          } # end hash
-        end # let
+            }
+          }
+        end
 
-        it 'should freeze the hash and child hashes' do
-          expect { instance.deep_freeze hsh }.
-            to change(hsh, :frozen?).
-            to be true
+        it 'should freeze the hash' do
+          expect { instance.deep_freeze hsh }
+            .to change(hsh, :frozen?)
+            .to be true
+        end
 
-          hsh.each do |_, child|
-            expect(child.frozen?).to be true
+        it 'should freeze the hash keys' do
+          instance.deep_freeze hsh
 
-            child.each do |_, value|
-              expect(value.frozen?).to be true
-            end # each
-          end # each
-        end # it
-      end # describe
-    end # shared_examples
+          expect(hsh.keys.all?(&:frozen?)).to be true
+        end
+
+        it 'should freeze the hash values' do
+          instance.deep_freeze hsh
+
+          expect(hsh.values.all?(&:frozen?)).to be true
+        end
+      end
+    end
 
     shared_examples 'should test if the hash is immutable' do
       describe 'with a hash with mutable keys' do
         let(:key_class) { Struct.new :value }
-        let(:hsh)       { { key_class.new('foo') => 'foo', key_class.new('bar') => 'bar', key_class.new('baz') => 'baz' } }
+        let(:hsh) do
+          {
+            key_class.new(+'foo') => +'foo',
+            key_class.new(+'bar') => +'bar',
+            key_class.new(+'baz') => +'baz'
+          }
+        end
 
         it { expect(instance.immutable? hsh).to be false }
 
-        wrap_context 'when the hash is frozen' do
+        context 'when the hash is frozen' do
+          include_context 'when the hash is frozen'
+
           it { expect(instance.immutable? hsh).to be false }
 
-          wrap_context 'when the hash keys are frozen' do
-            it { expect(instance.immutable? hsh).to be false }
-          end # wrap_context
+          context 'when the hash keys are frozen' do
+            include_context 'when the hash keys are frozen'
 
-          wrap_context 'when the hash values are frozen' do
             it { expect(instance.immutable? hsh).to be false }
-          end # wrap_context
+          end
+
+          context 'when the hash values are frozen' do
+            include_context 'when the hash values are frozen'
+
+            it { expect(instance.immutable? hsh).to be false }
+          end
 
           context 'when the hash keys and values are frozen' do
             include_context 'when the hash keys are frozen'
             include_context 'when the hash values are frozen'
 
             it { expect(instance.immutable? hsh).to be true }
-          end # context
-        end # wrap_context
-      end # describe
+          end
+        end
+      end
 
       describe 'with a hash with string keys' do
-        let(:hsh) { { 'foo' => 'foo', 'bar' => 'bar', 'baz' => 'baz' } }
+        let(:hsh) { { 'foo' => +'foo', 'bar' => +'bar', 'baz' => +'baz' } }
 
         it { expect(instance.immutable? hsh).to be false }
 
-        wrap_context 'when the hash is frozen' do
+        context 'when the hash is frozen' do
+          include_context 'when the hash is frozen'
+
           it { expect(instance.immutable? hsh).to be false }
 
-          wrap_context 'when the hash values are frozen' do
+          context 'when the hash values are frozen' do
+            include_context 'when the hash values are frozen'
+
             it { expect(instance.immutable? hsh).to be true }
-          end # wrap_context
-        end # wrap_context
-      end # describe
+          end
+        end
+      end
 
       describe 'with a hash with symbol keys' do
-        let(:hsh) { { :foo => 'foo', :bar => 'bar', :baz => 'baz' } }
+        let(:hsh) { { foo: +'foo', bar: +'bar', baz: +'baz' } }
 
         it { expect(instance.immutable? hsh).to be false }
 
-        wrap_context 'when the hash is frozen' do
+        context 'when the hash is frozen' do
+          include_context 'when the hash is frozen'
+
           it { expect(instance.immutable? hsh).to be false }
 
-          wrap_context 'when the hash values are frozen' do
+          context 'when the hash values are frozen' do
+            include_context 'when the hash values are frozen'
+
             it { expect(instance.immutable? hsh).to be true }
-          end # wrap_context
-        end # wrap_context
-      end # describe
+          end
+        end
+      end
 
       describe 'with a hash with hash values' do
         let(:hsh) do
           {
-            :english => {
-              'one'   => '1',
-              'two'   => '2',
-              'three' => '3'
-            }, # end hash
-            :japanese => {
-              'yon'  => '4',
-              'go'   => '5',
-              'roku' => '6'
-            }, # end hash
-            :spanish => {
-              'siete' => '7',
-              'ocho'  => '8',
-              'nueve' => '9'
-            } # end hash
-          } # end hash
-        end # let
+            english:  {
+              'one'   => +'1',
+              'two'   => +'2',
+              'three' => +'3'
+            },
+            japanese: {
+              'yon'  => +'4',
+              'go'   => +'5',
+              'roku' => +'6'
+            },
+            spanish:  {
+              'siete' => +'7',
+              'ocho'  => +'8',
+              'nueve' => +'9'
+            }
+          }
+        end
 
         it { expect(instance.immutable? hsh).to be false }
 
-        wrap_context 'when the hash is frozen' do
+        context 'when the hash is frozen' do
+          include_context 'when the hash is frozen'
+
           it { expect(instance.immutable? hsh).to be false }
 
-          wrap_context 'when the hash values are frozen' do
+          context 'when the hash values are frozen' do
+            include_context 'when the hash values are frozen'
+
             it { expect(instance.immutable? hsh).to be false }
 
             context 'when the hash child values are frozen' do
               before(:example) do
                 hsh.each { |_, child| child.each { |_, value| value.freeze } }
-              end # before example
+              end
 
               it { expect(instance.immutable? hsh).to be true }
-            end # context
-          end # wrap_context
-        end # wrap_context
-      end # describe
-    end # shared_examples
-  end # module
-end # module
+            end
+          end
+        end
+      end
+    end
+  end
+end
