636b24b1232ff0f4a7da18f18a3d50695e04490d
Rob Smith <merlin@sleepingkingstudios.com>
2020-07-13 17:54:22 -0400

    Deprecate sleeping_king_studios/tools/all.

--- lib/sleeping_king_studios/tools/all.rb
+++ lib/sleeping_king_studios/tools/all.rb
@@ -1,5 +1,10 @@
-# lib/sleeping_king_studios/tools/all.rb
+# frozen_string_literal: true
 
-Dir[File.join File.dirname(__FILE__), '*_tools.rb'].each do |file|
+require 'sleeping_king_studios/tools'
+
+SleepingKingStudios::Tools::CoreTools
+  .deprecate('sleeping_king_studios/tools/all')
+
+Dir[File.join File.dirname(__FILE__), '*_tools.rb'].sort.each do |file|
   require file
-end # end each
+end
