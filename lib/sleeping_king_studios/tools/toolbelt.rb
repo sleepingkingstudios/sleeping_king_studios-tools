a3d56c0448e9fac9ed390242b0f2ba4bb74cd7d5
Rob Smith <sleepingkingstudios@gmail.com>
2025-03-05 20:09:27 -0500

    Update Toolbelt documentation.

--- lib/sleeping_king_studios/tools/toolbelt.rb
+++ lib/sleeping_king_studios/tools/toolbelt.rb
@@ -4,6 +4,17 @@ require 'sleeping_king_studios/tools'
 
 module SleepingKingStudios::Tools
   # Helper object for quick access to all available tools.
+  #
+  # Specific tools can be accessed by name. For example, to access the
+  # configured instance of ArrayTools, call Toolbelt#array_tools. Certain tools
+  # also define a shorthand - for example, the array_tools instance can also be
+  # accessed using Toolbelt#ary.
+  #
+  # @example
+  #   toolbelt = SleepingKingStudios::Tools::Toolbelt.new
+  #
+  #   toolbelt.array_tools.pluralize('light')
+  #   #=> 'lights'
   class Toolbelt < BasicObject
     # @return [SleepingKingStudios::Tools::Toolbelt] a memoized instance of the
     #   toolbelt class.
@@ -38,24 +49,37 @@ module SleepingKingStudios::Tools
         ::SleepingKingStudios::Tools::StringTools.new(inflector:)
     end
 
+    # @return [SleepingKingStudios::Tools::ArrayTools] tools for working with
+    #   array-like enumerable objects.
     attr_reader :array_tools
+    alias ary array_tools
 
+    # @return [SleepingKingStudios::Tools::Assertions] methods for asserting on
+    #   the state of a function or application.
     attr_reader :assertions
 
+    # @return [SleepingKingStudios::Tools::CoreTools] tools for working with an
+    #   application or working environment.
     attr_reader :core_tools
 
+    # @return [SleepingKingStudios::Tools::HashTools] tools for working with
+    #   hash-like enumerable objects.
     attr_reader :hash_tools
-
-    attr_reader :integer_tools
-
-    attr_reader :object_tools
-
-    attr_reader :string_tools
-
-    alias ary array_tools
     alias hsh hash_tools
+
+    # @return [SleepingKingStudios::Tools::IntegerTools] tools for working with
+    #   integers.
+    attr_reader :integer_tools
     alias int integer_tools
+
+    # @return [SleepingKingStudios::Tools::ObjectTools] low-level tools for
+    #   working with objects.
+    attr_reader :object_tools
     alias obj object_tools
+
+    # @return [SleepingKingStudios::Tools::ObjectTools] tools for working with
+    #   strings.
+    attr_reader :string_tools
     alias str string_tools
 
     # @return [String] a human-readable representation of the object.
