d1e407614162d18bea4aa5f3cc8227d26877a2fb
Rob Smith <sleepingkingstudios@gmail.com>
2025-03-05 20:21:56 -0500

    Update ConstantMap documentation.

--- lib/sleeping_king_studios/tools/toolbox.rb
+++ lib/sleeping_king_studios/tools/toolbox.rb
@@ -3,9 +3,7 @@
 require 'sleeping_king_studios/tools'
 
 module SleepingKingStudios::Tools
-  # Namespace for common objects or patterns that are useful across projects but
-  # are larger than or do not fit the functional paradigm of the tools.*
-  # pattern.
+  # Namespace for defining common objects outside the tools.* pattern.
   module Toolbox
     autoload :ConstantMap,
       'sleeping_king_studios/tools/toolbox/constant_map'
