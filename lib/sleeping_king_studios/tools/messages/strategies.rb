4f79f04764a4484eff967505541af6187a19da49
Rob Smith <sleepingkingstudios@gmail.com>
2026-01-05 16:19:15 -0500

    Add input validation to Strategies::HashStrategy.

--- lib/sleeping_king_studios/tools/messages/strategies.rb
+++ lib/sleeping_king_studios/tools/messages/strategies.rb
@@ -5,6 +5,8 @@ require 'sleeping_king_studios/tools/messages'
 module SleepingKingStudios::Tools
   # Namespace for message strategy implementations.
   module Messages::Strategies
+    autoload :FileStrategy,
+      'sleeping_king_studios/tools/messages/strategies/file_strategy'
     autoload :HashStrategy,
       'sleeping_king_studios/tools/messages/strategies/hash_strategy'
   end
--- spec/sleeping_king_studios/tools/messages/strategies/hash_strategy_spec.rb
+++ spec/sleeping_king_studios/tools/messages/strategies/hash_strategy_spec.rb
@@ -16,12 +16,177 @@ RSpec.describe SleepingKingStudios::Tools::Messages::Strategies::HashStrategy do
     end
   end
 
+  describe '::ParseError' do
+    include_examples 'should define constant',
+      :ParseError,
+      -> { be_a(Class).and(be < StandardError) }
+  end
+
   describe '.new' do
     it { expect(described_class).to be_constructible.with(1).argument }
+
+    describe 'with nil' do
+      let(:error_message) do
+        'templates is not an instance of Hash'
+      end
+
+      it 'should raise an exception' do
+        expect { described_class.new(nil) }
+          .to raise_error ArgumentError, error_message
+      end
+    end
+
+    describe 'with an Object' do
+      let(:error_message) do
+        'templates is not an instance of Hash'
+      end
+
+      it 'should raise an exception' do
+        expect { described_class.new(Object.new.freeze) }
+          .to raise_error ArgumentError, error_message
+      end
+    end
+
+    describe 'with a Hash with nil key' do
+      let(:templates) do
+        {
+          'module_name' => 'Console Space Program',
+          nil           => "Ce n'est pas un message."
+        }
+      end
+      let(:error_message) do
+        'invalid key in templates - expected non-empty String or Symbol, got ' \
+          'nil'
+      end
+
+      it 'should raise an exception' do
+        expect { described_class.new(templates) }
+          .to raise_error described_class::ParseError, error_message
+      end
+    end
+
+    describe 'with a Hash with Object key' do
+      let(:invalid_key) { Object.new.freeze }
+      let(:templates) do
+        {
+          'module_name' => 'Console Space Program',
+          invalid_key   => "Ce n'est pas un message."
+        }
+      end
+      let(:error_message) do
+        'invalid key in templates - expected non-empty String or Symbol, got ' \
+          "#{invalid_key.inspect}"
+      end
+
+      it 'should raise an exception' do
+        expect { described_class.new(templates) }
+          .to raise_error described_class::ParseError, error_message
+      end
+    end
+
+    describe 'with a Hash with empty String key' do
+      let(:templates) do
+        {
+          'module_name' => 'Console Space Program',
+          ''            => "Ce n'est pas un message."
+        }
+      end
+      let(:error_message) do
+        'invalid key in templates - expected non-empty String or Symbol, got ' \
+          '""'
+      end
+
+      it 'should raise an exception' do
+        expect { described_class.new(templates) }
+          .to raise_error described_class::ParseError, error_message
+      end
+    end
+
+    describe 'with a Hash with empty Symbol key' do
+      let(:templates) do
+        {
+          'module_name' => 'Console Space Program',
+          :''           => "Ce n'est pas un message."
+        }
+      end
+      let(:error_message) do
+        'invalid key in templates - expected non-empty String or Symbol, got ' \
+          ':""'
+      end
+
+      it 'should raise an exception' do
+        expect { described_class.new(templates) }
+          .to raise_error described_class::ParseError, error_message
+      end
+    end
+
+    describe 'with a Hash with Object value' do
+      let(:invalid_value) { Object.new.freeze }
+      let(:templates) do
+        {
+          'module_name' => 'Console Space Program',
+          'rockets'     => invalid_value
+        }
+      end
+      let(:error_message) do
+        'invalid value in templates.rockets - expected Hash, Proc, or ' \
+          "String, got #{invalid_value.inspect}"
+      end
+
+      it 'should raise an exception' do
+        expect { described_class.new(templates) }
+          .to raise_error described_class::ParseError, error_message
+      end
+    end
+
+    describe 'with a Hash with nested invalid key' do
+      let(:templates) do
+        {
+          'module_name' => 'Console Space Program',
+          'rockets'     => {
+            'errors' => {
+              nil => "Ce n'est pas un message."
+            }
+          }
+        }
+      end
+      let(:error_message) do
+        'invalid key in templates.rockets.errors - expected non-empty String ' \
+          'or Symbol, got nil'
+      end
+
+      it 'should raise an exception' do
+        expect { described_class.new(templates) }
+          .to raise_error described_class::ParseError, error_message
+      end
+    end
+
+    describe 'with a Hash with nested invalid value' do
+      let(:invalid_value) { Object.new.freeze }
+      let(:templates) do
+        {
+          'module_name' => 'Console Space Program',
+          'rockets'     => {
+            'errors' => {
+              'invalid_key' => invalid_value
+            }
+          }
+        }
+      end
+      let(:error_message) do
+        'invalid value in templates.rockets.errors.invalid_key - expected ' \
+          "Hash, Proc, or String, got #{invalid_value.inspect}"
+      end
+
+      it 'should raise an exception' do
+        expect { described_class.new(templates) }
+          .to raise_error described_class::ParseError, error_message
+      end
+    end
   end
 
   describe '#call' do
-    deferred_examples 'should handle an invalid key' do
+    deferred_examples 'should handle a non-matching key' do
       context 'when the key does not match a template' do
         let(:key) do
           original = super()
@@ -35,7 +200,7 @@ RSpec.describe SleepingKingStudios::Tools::Messages::Strategies::HashStrategy do
     end
 
     deferred_examples 'should return the matching message' do
-      include_deferred 'should handle an invalid key'
+      include_deferred 'should handle a non-matching key'
 
       context 'when the key matches a template' do
         it { expect(call_strategy).to be == expected }
@@ -62,25 +227,25 @@ RSpec.describe SleepingKingStudios::Tools::Messages::Strategies::HashStrategy do
     end
 
     describe 'with key: a String' do
-      include_deferred 'should handle an invalid key'
+      include_deferred 'should handle a non-matching key'
     end
 
     describe 'with key: a Symbol' do
       let(:key) { super().to_sym }
 
-      include_deferred 'should handle an invalid key'
+      include_deferred 'should handle a non-matching key'
     end
 
     describe 'with key: a scoped value' do
       let(:key) { "errors.messages.#{super()}" }
 
-      include_deferred 'should handle an invalid key'
+      include_deferred 'should handle a non-matching key'
     end
 
     describe 'with options: value' do
       let(:options) { super().merge(ready: true) }
 
-      include_deferred 'should handle an invalid key'
+      include_deferred 'should handle a non-matching key'
     end
 
     describe 'with parameters: value' do
@@ -92,18 +257,18 @@ RSpec.describe SleepingKingStudios::Tools::Messages::Strategies::HashStrategy do
       end
       let(:options) { super().merge(parameters:) }
 
-      include_deferred 'should handle an invalid key'
+      include_deferred 'should handle a non-matching key'
     end
 
     describe 'with scope: value' do
       let(:options) { super().merge(scope: 'custom.scope') }
 
-      include_deferred 'should handle an invalid key'
+      include_deferred 'should handle a non-matching key'
 
       describe 'with key: a scoped value' do
         let(:key) { "errors.messages.#{super()}" }
 
-        include_deferred 'should handle an invalid key'
+        include_deferred 'should handle a non-matching key'
       end
     end
 
@@ -251,6 +416,124 @@ RSpec.describe SleepingKingStudios::Tools::Messages::Strategies::HashStrategy do
             },
             rockets: {
               launch_status: launch_status
+            }
+          },
+          module_name: 'Console Space Program'
+        }
+        templates
+      end
+      let(:expected) do
+        templates = {}
+        templates['module_name'] = 'Console Space Program'
+        templates['messages.errors.failure'] = 'not going to space'
+        templates['messages.rockets.launch_status'] = launch_status
+        templates
+      end
+
+      it { expect(strategy.templates).to be == expected }
+
+      it { expect(strategy.templates).to be_frozen }
+    end
+
+    context 'when initialized with templates: a Hash with nil values' do
+      let(:templates) do
+        templates = {
+          'messages'    => {
+            'errors'  => {
+              'failure' => 'not going to space'
+            },
+            'rockets' => {
+              'launch_status'   => launch_status,
+              'safety_briefing' => nil
+            }
+          },
+          'module_name' => 'Console Space Program'
+        }
+        templates
+      end
+      let(:expected) do
+        templates = {}
+        templates['module_name'] = 'Console Space Program'
+        templates['messages.errors.failure'] = 'not going to space'
+        templates['messages.rockets.launch_status'] = launch_status
+        templates
+      end
+
+      it { expect(strategy.templates).to be == expected }
+
+      it { expect(strategy.templates).to be_frozen }
+    end
+
+    context 'when initialized with templates: a Hash with empty Hash values' do
+      let(:templates) do
+        templates = {
+          'messages'    => {
+            'errors'  => {
+              'failure' => 'not going to space'
+            },
+            'rockets' => {
+              'launch_status'   => launch_status,
+              'safety_briefing' => {}
+            }
+          },
+          'module_name' => 'Console Space Program'
+        }
+        templates
+      end
+      let(:expected) do
+        templates = {}
+        templates['module_name'] = 'Console Space Program'
+        templates['messages.errors.failure'] = 'not going to space'
+        templates['messages.rockets.launch_status'] = launch_status
+        templates
+      end
+
+      it { expect(strategy.templates).to be == expected }
+
+      it { expect(strategy.templates).to be_frozen }
+    end
+
+    context 'when initialized with templates: a Hash with empty String values' \
+    do
+      let(:templates) do
+        templates = {
+          'messages'    => {
+            'errors'  => {
+              'failure' => 'not going to space'
+            },
+            'rockets' => {
+              'launch_status'   => launch_status,
+              'safety_briefing' => ''
+            }
+          },
+          'module_name' => 'Console Space Program'
+        }
+        templates
+      end
+      let(:expected) do
+        templates = {}
+        templates['module_name'] = 'Console Space Program'
+        templates['messages.errors.failure'] = 'not going to space'
+        templates['messages.rockets.launch_status'] = launch_status
+        templates
+      end
+
+      it { expect(strategy.templates).to be == expected }
+
+      it { expect(strategy.templates).to be_frozen }
+    end
+
+    context 'when initialized with templates: a Hash with empty Symbol values' \
+    do
+      let(:templates) do
+        templates = {
+          messages:    {
+            errors:  {
+              failure: 'not going to space'
+            },
+            rockets: {
+              launch_status:   launch_status,
+              safety_briefing: :''
             }
           },
           module_name: 'Console Space Program'
